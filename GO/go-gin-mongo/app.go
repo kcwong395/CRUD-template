74a8b3563b8441ef4f9ad382144effabe4ac542e
kcwong395 <kcwong395@gmail.com>
2021-07-14 03:00:32 -0400

    Add entry point and init gin engine

---
+++ GO/go-gin-mongo/app.go
@@ -0,0 +1,29 @@
+package main
+
+import (
+	"github.com/gin-gonic/gin"
+	"github.com/kcwong395/go-gin-mongo/api"
+	"github.com/kcwong395/go-gin-mongo/dbUtil"
+	"log"
+)
+
+func main() {
+
+	dbw, err := dbUtil.Init()
+	if err != nil {
+		panic(err)
+	}
+	defer dbw.Close()
+
+	// init gin
+	engine := gin.Default()
+
+	// define the route here
+	personApi := api.PersonApi{DBWrapper: dbw}
+	engine.GET("/people", personApi.GetPeople)
+
+	// Start the go server
+	if err := engine.Run(); err != nil {
+		log.Fatal(err.Error())
+	}
+}
